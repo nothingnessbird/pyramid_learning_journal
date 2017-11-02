872ea4040de2a17524eb7770d3bd165f7ae2457f
Kinley Ramson <kinleyramson@gmail.com>
2017-11-01 17:00:41 -0700

    added html and fixed routes.

--- pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/views/default.py
@@ -12,25 +12,25 @@ def list_view(request):
     """Serve all journal entries on home page."""
     path = os.path.join(HERE, '../templates/index.html')
     with io.open(path) as file:
-        return Response(file.read)
+        return Response(file.read())
 
 
 def detail_view(request):
     """Serve detail of journal entry."""
     path = os.path.join(HERE, '../templates/detail.html')
     with io.open(path) as file:
-        return Response(file.read)
+        return Response(file.read())
 
 
 def create_view(request):
     """Serve page to create new entry."""
     path = os.path.join(HERE, '../templates/new.html')
     with io.open(path) as file:
-        return Response(file.read)
+        return Response(file.read())
 
 
 def update_view(request):
     """Serve page to edit an existing entry."""
     path = os.path.join(HERE, '../templates/edit.html')
     with io.open(path) as file:
-        return Response(file.read)
+        return Response(file.read())
